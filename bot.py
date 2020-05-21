775d65c62c79206295522ac563cc4159dd46b83d
Emi <valadezn@uci.edu>
2020-05-20 23:03:13 -0700

    Added gitignore and established basic tweet-sending code

--- bot.py
+++ bot.py
@@ -4,5 +4,14 @@ Created on May 20, 2020
 @author: noemi
 '''
 
+from datetime import datetime
+import tweepy, credentials
+
+
 if __name__ == '__main__':
-    pass
+    
+    auth = tweepy.OAuthHandler(credentials.consumer_key, credentials.consumer_secret)
+    auth.set_access_token(credentials.access_token, credentials.access_token_secret)
+    api = tweepy.API(auth)
+    
+    api.update_status("test tweet")
