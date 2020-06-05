d1ec415cec8e2e96a8566fe181a3689cb5589d68
Emi <valadezn@uci.edu>
2020-06-04 23:46:16 -0700

    Fixed logging statement

--- bot.py
+++ bot.py
@@ -57,8 +57,7 @@ def updateTweet():
     ACCESS_SECRET = environ['ACCESS_SECRET']
     
     if (current_datetime.second == 0 and current_datetime.microsecond == 0):
-        print("bot.py: Current day: {}, {} --- Current time: {}:{}:{}:{}".format(todays_weekday, todays_weekday_str, current_datetime.hour, current_datetime.minute, 
-                                                                             current_datetime.second, current_datetime.microsecond))
+        print("bot.py: Current day: {}, {} --- Current time: {}".format(todays_weekday, todays_weekday_str, current_datetime.today()))
         sys.stdout.flush()
         logging.info("Current day: {}, {} --- Current time: {}:{}:{}:{}".format(todays_weekday, todays_weekday_str, current_datetime.hour, current_datetime.minute, 
                                                                             current_datetime.second, current_datetime.microsecond))
